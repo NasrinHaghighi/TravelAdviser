551a189982587f7922a1c925e328b6663dc971a2
nasrin haghighi <ns.haghighi@gmail.com>
2022-12-14 11:50:33 +0000

    map

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,36 @@ import List from './components/List/List';
 import { CssBaseline, Grid } from "@mui/material";
 import {getPlacesData} from './api/index'
 
+
+
 function App() {
 
   const [places, setPlaces] =useState<AxiosResponse | null | void>([] as any)
+  const [coordinates, setCordinates] =useState({})
+  const [bounds, setBounds] =useState<any>(coordinates)
+
+useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(function(position) {
+     setCordinates({lat:position.coords.latitude , lng:position.coords.longitude})
+  });
+},[])//to get user location onload//pass to map az center 
+
+
   useEffect(() => {
-    // getPlacesData().then((data)=>{
+    //   getPlacesData().then((data)=>{
     //   setPlaces(data)
     //   console.log(data)
     // })
-  }, [])
+  }, [bounds,coordinates])
+
+
   return (
     <div>
       <CssBaseline />
       <Navbar />
       <Grid container spacing={3} style={{width:'100%' }}>
         <Grid item xs={12} md={4}><List /></Grid>
-        <Grid item xs={12} md={8}><Map /></Grid>
+        <Grid item xs={12} md={8}><Map setCordinates={setCordinates} setBounds={setBounds} coordinates={coordinates}/></Grid>
       </Grid>
 
     
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,22 +1,47 @@
-import React,{useState} from 'react'
+import React,{useState, useRef, useCallback} from 'react'
 import GoogleMapReact from 'google-map-react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { current } from '@reduxjs/toolkit';
+
+const containerStyle = {
+  width: '1800px',
+  height: '80vh'
+};
+
+function Map({coordinates, setCordinates, setBounds}:any) {
 
 
+  const [mapref, setMapRef] = React.useState(null);
 
 
-function Map() {
- 
-    const defaultProps = {
-        center: {
-          lat: 10.99835602,
-          lng: 77.01502627
-        },
-        zoom: 11
-      };
-  return ( 
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyC9JxefVeuvmoE5bU1Wn1Vlg0gokKlMxxo"
+  })
+  if (!isLoaded){
+    return <h1>loading....</h1>
+  }
+
+  function handleLoad(map:any) {
+    setMapRef(map);
+  }
+const handelcenter =() =>{
+  if (mapref) {
+    const newCenter = mapref;
+    //console.log(newCenter.lat(), newCenter.lng())
+  }
+}
+
+ return ( 
   <div>
-map
-       
+    <GoogleMap center={coordinates} zoom={15} mapContainerStyle={containerStyle}
+        onLoad={handleLoad}
+        onCenterChanged	={handelcenter}
+     
+    >
+
+    </GoogleMap>
+      
  </div>
 )
 }
